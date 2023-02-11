fb7bd9482b78e217c555e64195d70688c9f6e96d
Koshy <55425558+ashish-koshy@users.noreply.github.com>
2023-02-11 20:54:03 +0530

    initial commit

---
+++ test.spec.ts
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+describe('ServerTests', function () {
+    it(`should fire a sample test`, function () {
+        assert.equal('Hello world!', 'Hello world!');
+    });
+});
